fbe30b2ee22721e19a8dbbc885c0093342385b55
Christopher John Kempf <kempch01@8269.dynamic.luther.edu>
2015-02-17 15:46:24 -0600

    more stuffs

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
     $('nav').addClass('shrink');
     $('nav .container-fluid').addClass('shrink');
+    $('nav .container-fluid form').addClass('shrink');
   } else {
     $('nav').removeClass('shrink');
     $('nav .container-fluid').removeClass('shrink');
+    $('nav .container-fluid form').removeClass('shrink');
   }
 });
