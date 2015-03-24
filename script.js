48a3360610b1efef75a013762793f495d91a1c30
norsecoders <kempch01@luther.edu>
2015-03-24 15:54:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+$("signupButton").click(function(){
+  var send_data = new FormData();
+  send_data.append("username", $('input[name=username]').val());
+  send_data.append("password", $('input[name=password]').val());
+  $.ajax({
+    type: "POST",
+    url: "127.0.0.1:8080/sp-backend1.0/newUser",
+    dataType: "json",
+    data: send_data,
+    success: function(response){
+      alert("reached success portion");
+    }
+  });
+});
