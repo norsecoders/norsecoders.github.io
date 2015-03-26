2792f66edcfbdce5b7bf62da377e3ebc5d577b4f
norsecoders <kempch01@luther.edu>
2015-03-26 15:34:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-$('div.aboutUs > div#main1 > div').submit(function(event){
-  event.preventDefault();
-  var $form = $(this),
-    url = $form.attr('action');
-    var posting = $.post( url, {username: $('#username').val(), password: $('#password').val()});
-    posting.done(function(data){
-      alert('success');
-    });
-});
+
