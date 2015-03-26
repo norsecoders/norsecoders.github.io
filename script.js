be0bd8881f21ea30430e5c42785d86e065616958
norsecoders <kempch01@luther.edu>
2015-03-26 15:20:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+$(#signupForm).submit(function(event){
+  event.preventDefault();
+  var $form = $(this),
+    url = $form.attr('action');
+    var posting = $.post( url, {username: $('#username').val(), password: $('#password').val()});
+    posting.done(function(data){
+      alert('success');
+    });
+});
