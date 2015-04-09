203c8dcdb249d25f8e5ed7a0021625ad8b38eeb3
norsecoders <kempch01@luther.edu>
2015-04-09 15:50:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ $(document).ready(function(){
   $('#loginForm').submit(function() {
     event.preventDefault();
     var loginArray = $(this).serializeArray();
-    var loginDict = $.map(loginArray, function(n,i){
-      return {Key: n.name, Value: $(n).val()};
+    var loginJSON = '{"username":'+'"'+signupArray[0]['value']+'"'+',"password":'+'"'+signupArray[1]['value']+'"}'
+    $.ajax({
+      url:'/sp-backend1.0/api/login/',
+      type:"POST",
+      contentType:"application/json; charset=utf-8",
+      data:signupJSON,
+      dataType:"jsonp"
     });
-    console.log(loginDict);
   });
 
   $('#signupForm').submit(function(){
     event.preventDefault();
     var signupArray = $(this).serializeArray();
     var signupJSON = '{"username":'+'"'+signupArray[0]['value']+'"'+',"password":'+'"'+signupArray[1]['value']+'"}'
-    console.log(signupJSON);
     $.ajax({
       url:'/sp-backend1.0/api/newUser/',
       type:"POST",
